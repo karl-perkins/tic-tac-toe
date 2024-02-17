e5f6b7d1632457a2c94d0a4b7ffabe7ea3b31d06
Karl Perkins <karl@karlperkins.me>
2024-02-18 11:01:45 +1300

    Add player and gamecontroller logic

--- script.js
+++ script.js
@@ -8,25 +8,82 @@ function createGameboard() {
 		}
 	}
 
-	function displayGameboard() {
-		console.log(gameboard[0].map((cell) => cell.getValue()).join('|'));
-		console.log(gameboard[1].map((cell) => cell.getValue()).join('|'));
-		console.log(gameboard[2].map((cell) => cell.getValue()).join('|'));
+	function displayBoard() {
+		console.log(gameboard[0].map((cell) => cell.getValue()).join(' | '));
+		console.log(gameboard[1].map((cell) => cell.getValue()).join(' | '));
+		console.log(gameboard[2].map((cell) => cell.getValue()).join(' | '));
 	}
 
-	function updateGameboard(rowId, columnId, value) {
+	function setCellValue(rowId, columnId, value) {
 		gameboard[rowId][columnId].setValue(value);
 	}
 
-	return { displayGameboard, updateGameboard };
+	function getCellValue(rowId, columnId) {
+		return gameboard[rowId][columnId].getValue();
+	}
+
+	return { displayBoard, setCellValue, getCellValue };
 }
 
 function createCell() {
-	let value = 0;
+	let value = ' ';
 	const getValue = () => value;
 	const setValue = (player) => (value = player);
 
-	return { value, getValue, setValue };
+	return { getValue, setValue };
 }
 
-const gameboard = createGameboard();
+function createPlayer(name, marker) {
+	let winCount = 0;
+	const getWinCount = () => winCount;
+	const setWinCount = () => winCount++
+	return { name, marker, getWinCount, setWinCount };
+}
+
+const gameController = (function (name1, name2) {
+	const gameboard = createGameboard();
+	const players = [ createPlayer(name1, 'X'), createPlayer(name2, 'O') ];
+	const winCombos = [
+		//Row
+		[[0, 0], [0, 1], [0, 2]],
+		[[1, 0], [1, 1], [1, 2]],
+		[[2, 0], [2, 1], [2, 2]],
+
+		//Column
+		[[0, 0], [1, 0], [2, 0]],
+		[[0, 1], [1, 1], [2, 1]],
+		[[0, 2], [1, 2], [2, 2]],
+		
+		//Diagonal
+		[[0, 0], [1, 1], [2, 2]],
+		[[0, 2], [1, 1], [2, 0]],
+	];
+
+	function playRound() {
+		let activePlayer;
+		let isTrue = true;
+		
+		while (isTrue) {
+			activePlayer = (activePlayer === players[0]) ? players[1] : players[0];
+
+			// Add handling for invalid input and check if cell already populated.
+			const row = window.prompt(`${activePlayer.name}, choose a row:`);
+			const column = window.prompt(`${activePlayer.name}, choose a column:`);
+			
+
+			gameboard.setCellValue(row, column, activePlayer.marker);
+
+			gameboard.displayBoard();
+
+			const isWin = winCombos.some(combo => combo.every(cell => gameboard.getCellValue(cell[0], cell[1]) === activePlayer.marker));
+			if (isWin) {
+				isTrue = false;
+				activePlayer.setWinCount();
+				console.log(`${activePlayer.name} wins!`);
+			}
+		}
+	}
+
+	return { playRound };
+
+})('karl', 'simon')
